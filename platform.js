9a08fe7e3d111b11efdf3b3d890f38a26bda07c3
Rajesh Pany <rajesh.pany.ext@wsa.com>
2022-12-20 22:31:52 +0530

    changes platform.js

--- platform.js
+++ platform.js
@@ -7,88 +7,94 @@ function getMobileOperatingSystem() {
 
   // Windows Phone must come first because its UA also contains "Android"
   if (/windows phone/i.test(userAgent)) {
-      return "Windows Phone";
+    return "Windows Phone";
   }
 
   if (/android/i.test(userAgent)) {
-      return "Android";
+    return "Android";
   }
 
   // iOS detection from: http://stackoverflow.com/a/9039885/177710
   if (/iPad|iPhone|iPod/.test(userAgent)) {
-      return "iOS";
+    return "iOS";
   }
 
   return "unknown";
 }
 
- function getOSInfo() {
+function getOSInfo() {
 
   let OSName = "Unknown OS";
   if (!isMobile) {
-      if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
-      if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
-      if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
-      if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
+    if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+    if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+    if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+    if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
   }
   else {
-      OSName = getMobileOperatingSystem();
+    OSName = getMobileOperatingSystem();
   }
 
   return OSName
 }
-  
- function getBrowserName() {
-    // The order matters here, and this may report false positives for unlisted browsers.          
-  let userAgent=navigator.userAgent;
 
-   if (userAgent.includes("Firefox")) {
-      // "Mozilla/5.0 (X11; Linux i686; rv:104.0) Gecko/20100101 Firefox/104.0"
-      return "Mozilla Firefox";
-    } else if (userAgent.includes("SamsungBrowser")) {
-      // "Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36"
-      return "Samsung Internet";
-    } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
-      // "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 OPR/90.0.4480.54"
-      return "Opera";
-    } else if (userAgent.includes("Trident")) {
-      // "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)"
-      return "Microsoft Internet Explorer";
-    } else if (userAgent.includes("Edge")) {
-      // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
-      return "Microsoft Edge (Legacy)";
-    } else if (userAgent.includes("Edg")) {
-      // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70"
-      return "Microsoft Edge (Chromium)";
-    } else if (userAgent.includes("Chrome")) {
-      // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-      return "Google Chrome or Chromium";
-    } else if (userAgent.includes("Safari")) {
-      // "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1"
-      return "Apple Safari";
-    } else {
-      return "unknown";
-    } 
+function getBrowserName() {
+  // The order matters here, and this may report false positives for unlisted browsers.          
+  let userAgent = navigator.userAgent;
+
+  if (userAgent.includes("Firefox")) {
+    // "Mozilla/5.0 (X11; Linux i686; rv:104.0) Gecko/20100101 Firefox/104.0"
+    return "Mozilla Firefox";
+  } else if (userAgent.includes("SamsungBrowser")) {
+    // "Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36"
+    return "Samsung Internet";
+  } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
+    // "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 OPR/90.0.4480.54"
+    return "Opera";
+  } else if (userAgent.includes("Trident")) {
+    // "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)"
+    return "Microsoft Internet Explorer";
+  } else if (userAgent.includes("Edge")) {
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+    return "Microsoft Edge (Legacy)";
+  } else if (userAgent.includes("Edg")) {
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70"
+    return "Microsoft Edge (Chromium)";
+  } else if (userAgent.includes("Chrome")) {
+    // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+    return "Google Chrome or Chromium";
+  } else if (userAgent.includes("Safari")) {
+    // "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1"
+    return "Apple Safari";
+  } else {
+    return "unknown";
+  }
 }
 
- function DeviceType()
-{
-  if(!isMobile && !isTablet)
-  {
+function DeviceType() {
+  if (!isMobile && !isTablet) {
     return "Desktop/Laptop";
   }
-  else
-  {
-    return isTablet ? "Tablet" : "Smartphone";
+  else {
+    return checkTabletFromWindowScreenSize();
   }
 }
-  
+
 function getYouCode() {
-    document.getElementById('isMobile').innerHTML = "DeviceType:" + DeviceType();
-    document.getElementById('osName').innerHTML = getOSInfo();
-    document.getElementById('browserInfo').innerHTML = getBrowserName();
+  document.getElementById('isMobile').innerHTML = "DeviceType:" + DeviceType();
+  document.getElementById('osName').innerHTML = getOSInfo();
+  document.getElementById('browserInfo').innerHTML = getBrowserName();
 }
 
 window.onload = function () {
-	getYouCode();
+  getYouCode();
+}
+
+function checkTabletFromWindowScreenSize() {
+  let viewportHeight = window.innerHeight;
+  let viewportWidth = window.innerWidth;
+  if (viewportWidth > 350 && viewportHeight > 620) {
+    return "Smartphone";
+  }
+  return "Tablet";
 }
